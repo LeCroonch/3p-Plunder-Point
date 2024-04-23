c9561c21d2998b06ce232079d05368e80b2ebebe
LeCroonch <alejandro.fernandez2001@yahoo.com>
2024-04-22 21:10:46 -0400

    Revert "Hopefully works"
    This reverts commit f3ca348be2626b5c18a497efc60b50aef9fde12f.

--- Source/CAP4053Game/CAP4053Game.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CAP4053Game.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CAP4053Game, "CAP4053Game" );
- 
